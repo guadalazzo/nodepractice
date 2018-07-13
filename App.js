5f4d56fb7e757ea9608c9abc23baa38abff563de
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2018-07-13 15:13:02 -0300

    advaced on server with pug

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 
-const server = http.createServer((req, res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello World!');
+mongoose.connect('mongodb://localhost/articles');
+let db = mongoose.connection;
+
+//Check connection
+db.once('open',()=>{
+    console.log('Connected to MongoDB');
 });
-server.listen(port, hostname,() => {
-    console.log('server started on port:'+ port);
+
+//chech for db errors
+db.on('error',(err)=>{
+    console.log(err);
+});
+//Init app
+const app =  express();
+//Load view Engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','pug');
+//Home Route
+app.get('/', (req,res)=>{
+    //res.send('hello world');//send something to the browser
+    //array harcoded for working pug
+    /*let articles = [
+        {4
+            id: 1,
+            title: 'Article one',
+            author: 'Brad Traversy',
+            body: 'This is article one'
+        },
+        {
+            id: 2,
+            title: 'Article two',
+            author: 'Joe Doe',
+            body: 'This is article two'
+        },
+        {
+            id: 3,
+            title: 'Article three',
+            author: 'Jhon Doe',
+            body: 'This is article three'
+        },
+    ]*/
+
+    res.render('index', {
+        title:'Articles',
+        articles: articles
+    });//render index
+});
+
+//Add route
+app.get('/articles/add', (req,res)=> {
+    res.render('add_articles',{
+        title:'Add article'
+    });
+});
+
+//Start server
+app.listen(3000,()=>{
+    console.log('server started on port 3000...');//escucha el puerto.
 });
